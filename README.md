8a3a92ddc74b826c498e7a2f61e53aa7e1856e7e
James Clark <james.clark@sky.uk>
2019-06-27 07:25:57 +0100

    change to pass airbnb calendar url as query param

--- README.md
+++ README.md
@@ -5,15 +5,13 @@
 npm install
 ```
 
-# Configure
-Create a `calendars.json` file with an array of the Airbnb .ics URLs you'd like to use"
-```
-[
-    "https://www.airbnb.co.uk/calendar/ical/12345678.ics?s=asd875as8as8g5d7sg65"
-]
-```
-
 # Run
 ```
-npm run prod
+npm start
+```
+
+# Use
+Pass the Airbnb calendar URL in the `url` query parameter of this API like:
+```
+localhost:3000/?url=https://www.airbnb.co.uk/calendar/ical/12345678.ics?s=asd875as8as8g5d7sg65
 ```
--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import calendars from "./calendars.json"
 const app = express()
 const port = parseInt(process.env.PORT || "3000")
 
-app.get("/", async (_req, res) => {
+app.get("/", async (req, res) => {
     const cal = icalGenerator({
         prodId: {
             company: 'Airbnb Inc',
@@ -16,21 +16,18 @@ app.get("/", async (_req, res) => {
         }
     })
 
-    for (const calendar in calendars) {
-        const resp = await axios.get(calendar)
-        ical.parseICS(resp.data, function (err, data) {
-            if (err) console.log(err)
-            for (const key in data) {
-                if (key === 'prodid') continue
-                if (data.hasOwnProperty(key) && data[key].summary !== 'Airbnb (Not available)') {
-                    cal.createEvent(data[key])
-                }
+    const { url } = req.query
+    const resp = await axios.get(url)
+    ical.parseICS(resp.data, function (err, data) {
+        if (err) console.log(err)
+        for (const key in data) {
+            if (key === 'prodid') continue
+            if (data.hasOwnProperty(key) && data[key].summary !== 'Airbnb (Not available)') {
+                cal.createEvent(data[key])
             }
-
-        });
-    }
-
-    res.send(cal.toString())
+        }
+        res.send(cal.toString())
+    })
 });
 
 // start the Express server
